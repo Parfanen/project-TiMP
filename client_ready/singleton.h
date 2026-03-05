ebd68b1d59c64bb0b8d071cb11437ba87026f9c5
Gleb710 <gleb527gleb@gmail.com>
2026-03-05 14:06:24 +0300

    Create singleton.h

---
+++ client_ready/singleton.h
@@ -0,0 +1,45 @@
+class Singleton;
+
+// Для удаления данных
+class SingletonDestroyer 
+{
+    private:
+        Singleton * p_instance;
+    public:
+        ~SingletonDestroyer() 
+        {
+            delete p_instance;
+        }
+        void initialize(Singleton * p) 
+        {
+            p_instance = p;
+        }
+};
+
+class Singleton
+{
+    private:
+        static Singleton * p_instance;
+        static SingletonDestroyer destroyer;
+    protected:
+        Singleton() {}
+        Singleton(const Singleton&) = delete;
+        Singleton& operator = (Singleton &) = delete;
+        ~Singleton() {}
+        friend class SingletonDestroyer;
+    public:
+        static Singleton* getInstance() 
+        {
+            if (!p_instance) 
+            {
+                p_instance = new Singleton();
+                destroyer.initialize(p_instance);
+            }
+            return p_instance;
+        }
+};
+
+// Выделение памяти под статичные переменные
+Singleton::Singleton * p_instance;
+Singleton::SingletonDestroyer destroyer;
+
